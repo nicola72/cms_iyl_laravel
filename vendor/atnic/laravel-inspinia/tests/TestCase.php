3ac0b4b7761d4763d9c5d7f460523b4a6b89399f
Nicola <nicola.tamburini@fjstudio.com>
2020-03-10 13:04:23 +0100

    commit

---
+++ vendor/atnic/laravel-inspinia/tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Atnic\Inspinia\Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+}
